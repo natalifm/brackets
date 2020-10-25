e01a76f59905ffa3291484a1c552f93666cebef7
Nataliia Ometiukh <nataliia.ometiukh@gmail.com>
2020-10-25 23:43:02 +0200

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const newConfigArr = bracketsConfig.map(function (item) {
+        return item.join("");
+    })
+    const reducedLength = str.length / 2;
+    let i = 0;
+    while (i < reducedLength) {
+        for (let j = 0; j < newConfigArr.length; j++) {
+            str = str.replace(newConfigArr[j], "");
+        }
+        i++;
+    }
+    return (str.length === 0);
 }
